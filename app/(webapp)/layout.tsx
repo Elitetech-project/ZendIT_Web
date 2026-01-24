b1aba866f35bb0b20451f14e564ebae47f50e3f6
chiscookeke11 <chiscookeke11@gmail.com>
2026-01-24 22:22:01 +0100

    remove use mobile hook to allow display on desktop devices

--- app/(webapp)/layout.tsx
+++ app/(webapp)/layout.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import OnboardingScreen from "@/components/Onboarding"
-import { useMobile } from "@/hooks/use-mobile"
 import { useEffect, useState } from "react"
 
 
@@ -9,7 +8,6 @@ import { useEffect, useState } from "react"
 
 
 export default function WebAppLayout({ children }: { children: React.ReactNode }) {
-    const { isLoading: mobileLoading, isMobile } = useMobile()
     const [showOnBoarding, setShowOnboarding] = useState(false)
 
 
@@ -34,29 +32,8 @@ export default function WebAppLayout({ children }: { children: React.ReactNode }
 
 
 
-    if (mobileLoading) {
-        return (
-            <div>Loading ...</div>
-        )
-    }
 
-
-    if (!isMobile) {
-        return (
-            <div className="flex h-screen items-center justify-center w-full flex-col gap-4 p-4 ">
-                <h1 className="text-2xl text-[#EF8F57] font-bold font-merriweather">Desktop view is not supported.</h1>
-                <p className="text-sm text-[#1e1e1e]  font-lato ">Please switch to a mobile for better experience</p>
-                <div className="w-fit flex items-center gap-5" >
-                </div>
-            </div>
-        );
-    }
-
-
-
-
-
-    if (isMobile && showOnBoarding) {
+    if (showOnBoarding) {
         return (
             <OnboardingScreen onFinish={handleFinish} />
         )
