09a336d2562cad065a0c244cf1f12d4111828606
chiscookeke11 <chiscookeke11@gmail.com>
2026-01-31 05:23:08 +0100

    implemented the auth pages

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+
+
+// Input change function
+export const handleChange = <T extends Record<string, any>>(
+  e: React.ChangeEvent<HTMLInputElement>,
+  setFormValues: React.Dispatch<React.SetStateAction<T>>
+) => {
+  const { name, value } = e.target;
+
+  if (name === "phoneNumber" && isNaN(Number(value))) return;
+
+  setFormValues((prev) => ({
+    ...prev,
+    [name]: value,
+  }));
+};
